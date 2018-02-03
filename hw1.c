e526d03b67216504d693843533549956ec679160
mpedra230 <35666664+mpedra230@users.noreply.github.com>
2018-02-02 19:06:46 -0600

    Add files via upload
    This is my homework 1 code.  I couldn't upload it to my hw1 repository.

---
+++ hw1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("%s%d", "mpedra", 5);   
+   return 0;
+}
+
+void all(){}
+int your;
+void cs361(){}
+char are[1] = {'a'};
+int belong;
+int to = 0; 
+char us[37];
